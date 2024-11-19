86b1b6262fe7b7d9482365f639b634a09d23b46f
Prateek Bhindwar <pbprateeek@gmail.com>
2024-11-19 15:51:46 -0500

    Sequential TA model

--- src/TACNNModel/TACNN.py
+++ src/TACNNModel/TACNN.py
@@ -9,59 +9,50 @@ from utils.layers import *
 
 # Define the CNN model
 class TACNN(nn.Module):
-    def __init__(self, alpha, temperature):
+    def __init__(self, alpha, temperature, num_of_classes):
         super(TACNN, self).__init__()
-        self.conv1_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=1, padding=1)
-        self.activation_fn1_1 = nn.ReLU()
-        self.conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=1, padding=1)
-        self.activation_fn1_2 = nn.ReLU()
-        self.maxPool1 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        
-        self.conv2_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1)
-        self.activation_fn2_1 = nn.ReLU()
-        self.conv2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=1, padding=1)
-        self.activation_fn2_2 = nn.ReLU()
-        self.maxPool2 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        
-        self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=1, padding=1)
-        self.activation_fn3_1 = nn.ReLU()
-        self.conv3_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1)
-        self.activation_fn3_2 = nn.ReLU()
-        self.conv3_3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1)
-        self.maxPool3 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        
-        self.flatten_layer = nn.Flatten()
-        self.fc1 = nn.Linear(2058, 1024)  
-        self.activation_fn4 = nn.ReLU()# assuming input images are 32x32
-        self.fc2 = nn.Linear(1024, 1000)
-        self.classifier_activation = nn.Softmax()  # assuming 10 classes
-        
         self.alpha = alpha
         self.temperature = temperature
+        self.network = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)),
 
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)),
+
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)),
+            
+            nn.Linear(200704, 50176),
+            nn.BatchNorm1d(50176),
+            nn.ReLU(),
+            nn.Linear(50176, 4096),
+            nn.BatchNorm1d(4096),
+            nn.ReLU(),
+            nn.Linear(4096, 4096),
+            nn.BatchNorm1d(4096),
+            nn.ReLU(),
+            nn.Linear(4096, num_of_classes)
+        )
     def forward(self, x):
-        x = self.activation_fn1_1(self.conv1_1(x))
-        x = self.activation_fn1_2(self.conv1_2(x))
-        x = self.maxPool1(x)
-
-        x = self.activation_fn2_1(self.conv2_1(x))
-        x = self.activation_fn2_2(self.conv2_2(x))
-        x = self.maxPool2(x)
-
-        x = self.activation_fn3_1(self.conv3_1(x))
-        x = self.activation_fn3_2(self.conv3_2(x))
-        x = self.activation_fn3_2(self.conv3_3(x))
-        x = self.maxPool3(x)  
-
-        x = self.flatten_layer(x)
-
-        x = self.fc1(x)
-        x = self.activation_fn4(x)
-
-        x = self.fc2(x)
-        x = self.classifier_activation(x)
-        
-        return x
-    
+        return self.network(x)
     def risk(self, Y, teacher_preds, output):
         return loss_l2(Y, output) + loss_kl_divergence(output, Y, teacher_preds, alpha=self.alpha, temperature=self.temperature)
